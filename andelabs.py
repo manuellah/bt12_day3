18f6558f371d86f40cc3fcf4115d091c78c10d23
manuellah <emmanumuthui@gmail.com>
2016-11-12 13:12:22 +0300

    successsfully implimented find missing algorithm

--- andelabs.py
+++ andelabs.py
@@ -1,3 +1,20 @@
+def find_missing(list1, list2):
+    if not list1 and not list2:
+        return 0
+    elif list1 == list2:
+        return 0
+    elif not (isinstance(list1, list) or isinstance(list1, list)):
+        return 0
+    elif len(list1) > len(list2):
+        set_difference = set(list1) - set(list2)
+    elif len(list2) > len(list1):
+        set_difference = set(list2) - set(list1)
+    else:
+        return 0
+    
+    return set_difference[0]
+
+
 class BinarySearch():
     def __init__(self, a, b):
         self.length = a
@@ -30,9 +47,7 @@ class BinarySearch():
                 else:
                     first = midpoint + 1
         
-        return 'not found'
+        result_dict['count'] = 3
+        result_dict['index'] = -1
+        return result_dict
     
-bs = BinarySearch(20,2)
-print len(bs.created_list)
-print bs.created_list
-print bs.search(4)
